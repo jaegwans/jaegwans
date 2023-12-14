ef5898e17f534bf96db1c093150d8bac074c4214
김재관 <50818389+jaegwans@users.noreply.github.com>
2023-12-14 17:41:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ MAX_POST = 5
 
 markdown_text = """
 
-* Major in software engineering
-* <a href="https://github.com/GDSC-SKHU">GDSC SKHU 1st- Web member</a>
-* <a href="https://user-images.githubusercontent.com/50818389/208008419-a0706e72-0ee6-4ab2-957c-deed0abeded4.png">Comento 프론트엔드 직무캠프 수료</a>
+
 
 ### 📭 Contect 
 skhu201811140@gmail.com
@@ -19,16 +17,7 @@ https://bit.ly/3GLBMFW
 ### 👨🏻‍💻 blog
 http://blog.jaegwan.com
 
-### Skill
-<img src="https://img.shields.io/badge/React-9cf?style=flat-square&logo=react&logoColor=white"/>  <img src="https://img.shields.io/badge/Next.js-000?style=flat-square&logo=next.js&logoColor=white"/> <img src="https://img.shields.io/badge/javascript-yellow?style=flat-square&logo=javascript&logoColor=white"/> 
-<img src="https://img.shields.io/badge/typescript-blue?style=flat-square&logo=typescript&logoColor=skyblue"/><img src="https://img.shields.io/badge/redux-white?style=flat-square&logo=redux&logoColor=purple"/>
-<img src="https://img.shields.io/badge/styled_components-black?style=flat-square&logo=styledcomponents&logoColor=pink"/> <img src="https://img.shields.io/badge/recoil-grey?style=flat-square&logo=recoil&logoColor=skyblue"/>
-<img src="https://img.shields.io/badge/react_query-black?style=flat-square&logo=reactquery&logoColor=skyblue"/>
-<img src="https://img.shields.io/badge/ReactNative-ccf?style=flat-square&logo=react&logoColor=white"/>
-</br><img src="https://img.shields.io/badge/Spring-green?style=flat-square&logo=Spring&logoColor=white"/>
-    <img src="https://img.shields.io/badge/MySQL-4479A1?style=flat&logo=MySQL&logoColor=white">
-<img src="https://img.shields.io/badge/Redis-DC382D?style=flat&logo=Redis&logoColor=white">
-<img src="https://img.shields.io/badge/SQLite-003B57?style=flat&logo=SQLite&logoColor=white">
+
 
 ### ✅ Latest Blog Post
 
